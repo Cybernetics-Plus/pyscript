a2e20b9f542c2c4272bea261c57c3fbe86afa965
BOMEN <bomen21x@gmail.com>
2022-11-19 20:18:56 +0700

    Update __manifest__.py

--- ctp_pyscript/__manifest__.py
+++ ctp_pyscript/__manifest__.py
@@ -7,37 +7,37 @@
 ###################################################################################
 
 {
-    'name': 'Pyscript',
-    'version': '15.0.0.0.1',
-    'summary': """ 
+    "name": "Pyscript",
+    "version": "15.0.0.0.1",
+    "summary": """ 
             Install Pyscript in Odoo Frontend
             .""",
-    'description': """ 
+    "description": """ 
             Install Pyscript in Odoo Frontend
             .""",
-    'author': 'Cybernetics Plus Co., Ltd.',
-    'website': 'https://www.cybernetics.plus',
-    'live_test_url': 'https://www.cybernetics.plus',
-    'images': ['static/description/banner.gif'],
-    'category': 'Website',
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': True,
-    'auto_install': False,
-    'contributors': [
-        'Developer <dev@cybernetics.plus>',
+    "author": "Cybernetics+",
+    "website": "https://www.cybernetics.plus",
+    "live_test_url": "https://www.cybernetics.plus",
+    "images": ["static/description/banner.gif"],
+    "category": "Website",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "contributors": [
+        "Developer <dev@cybernetics.plus>",
     ],
-    'depends': [
-        'website',
+    "depends": [
+        "website",
     ],
-    'assets': {
-        'web.assets_qweb': [
-            'https://pyscript.net/latest/pyscript.css',
-            'https://pyscript.net/latest/pyscript.js',
+    "assets": {
+        "web.assets_qweb": [
+            "https://pyscript.net/latest/pyscript.css",
+            "https://pyscript.net/latest/pyscript.js",
         ],
-        'web.assets_frontend': [
-            'https://pyscript.net/latest/pyscript.css',
-            'https://pyscript.net/latest/pyscript.js',
+        "web.assets_frontend": [
+            "https://pyscript.net/latest/pyscript.css",
+            "https://pyscript.net/latest/pyscript.js",
         ],
     },
 }
